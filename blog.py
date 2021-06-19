0a23deb19a054c5391d37ae862ac2ecba7f083c1
Strfr <aawifoa@gmail.com>
2021-06-19 23:11:36 +0300

    Brief way of passing data from  python to html

--- blog.py
+++ blog.py
@@ -4,7 +4,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    number=10
+    return render_template("index.html",num = number)
 @app.route("/about")
 def about():
     return "about"
