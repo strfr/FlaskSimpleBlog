07d40fcb2c95511f11a7af0166fdaf8da4d4e6d5
Strfr <aawifoa@gmail.com>
2021-06-20 13:45:34 +0300

    Added Bootstrap navbar with extend and include.

--- blog.py
+++ blog.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     article =dict()
-    return render_template("about.html")
-@app.route("/about")
-def about():
-    return "about"
+    return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True) 
