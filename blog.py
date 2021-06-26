b1041ba460a951f578b4d748e8f6766c4e78dd1e
Strfr <aawifoa@gmail.com>
2021-06-26 12:24:44 +0300

    Jinja if elif else on flask
    jinja if else usage briefly made.

--- blog.py
+++ blog.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     article =dict()
-    return render_template("index.html")
+    return render_template("index.html", process = 1)
+@app.route("/about")
+def about():
+    return render_template("about.html")
 if __name__ == "__main__":
     app.run(debug=True) 
