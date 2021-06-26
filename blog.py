ab8dffa127a69a7ade74b20e2dd01ac4de956355
Strfr <aawifoa@gmail.com>
2021-06-26 12:42:33 +0300

    for each on Jinja template implemented on dictionaries

--- blog.py
+++ blog.py
@@ -4,8 +4,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    article =dict()
-    return render_template("index.html", process = 1)
+    articles = [
+        {"id":1, "title":"Title1","content":"Content1"},
+        {"id":2, "title":"Title2","content":"Content2"},
+        {"id":3, "title":"Title3","content":"Content3"}
+    ]
+    return render_template("index.html", articles = articles)
 @app.route("/about")
 def about():
     return render_template("about.html")
